1ad942ce1eef6dfca52c8cce5644cdbc1d9ed301
Abdullahkal <abbdullah.y22@gmail.com>
2025-05-10 23:45:05 +0300

    Add products migration and fix deploy commands

--- database/migrations/2025_05_10_175157_create_products_table.php
+++ database/migrations/2025_05_10_175157_create_products_table.php
@@ -7,19 +7,20 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name'); // اسم المنتج (مثل بيرا، نوفا، العين)
-            $table->text('description')->nullable(); // وصف اختياري
-            $table->integer('quantity')->default(0); // الكمية المتوفرة
-            $table->decimal('price', 8, 2)->default(0.00); // سعر المنتج
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->text('description')->nullable();
+        $table->integer('quantity');
+        $table->decimal('price', 8, 2);
+        $table->string('image')->nullable();
+        $table->timestamps();
+    });
+}
 
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
-    }
+public function down(): void
+{
+    Schema::dropIfExists('products');
+}
 };
